c6e93258c75bacc58c8d655632dc96341714a699
computej <computejpdc@gmail.com>
2024-10-02 11:23:06 -0700

    make a bunch of tests not use date.now

--- __tests__/agecalc.test.js
+++ __tests__/agecalc.test.js
@@ -25,7 +25,7 @@ describe("UserAgeObject", () => {
   });
   describe("getAgeOnOtherPlanets()", () => {
     test("It should return a set=like object" , () => {
-      let testAgeObject = new UserAgeObject(Date.now()); 
+      let testAgeObject = new UserAgeObject(testFirstDate, testSecondDate);
       let testMap = testAgeObject.getAgeOnOtherPlanets();
       //These two functions are in both JS sets and maps
       //Don't use union() et al because thats very recently implemented in browsers
@@ -33,7 +33,7 @@ describe("UserAgeObject", () => {
       expect(testMap.keys.toString()).toBeTruthy();
     });
     test("The returned object should have the following keys: \nMercury\nVenus\nMars\nJupiter" , () => {
-      let testAgeObject = new UserAgeObject(Date.now());
+      let testAgeObject = new UserAgeObject(testFirstDate, testSecondDate);
       let testMap = testAgeObject.getAgeOnOtherPlanets();
       expect(testMap.has("Mercury")).toBeTruthy();
       expect(testMap.has("Venus")).toBeTruthy();
