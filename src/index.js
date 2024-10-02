6d33da1e2d755bf5a0e4d3d094debbeee2373b7f
computej <computejpdc@gmail.com>
2024-10-02 11:24:11 -0700

    implement the undefined argument check

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 export class UserAgeObject {
+
   constructor(dateOne,dateTwo) {
-    this.firstDate = dateOne;
-    this.secondDate = dateTwo;
+      this.firstDate = dateOne;
+      if(!dateTwo) { 
+        this.secondDate = Date.now();
+      } else {
+        this.secondDate = dateTwo;
+      }
+      if (this.firstDate.valueOf() >= this.secondDate.valueOf()) {
+        throw new Error('The first date must be chronologically before the second date');
+      }
   }
 
   getAgeOnOtherPlanets() {
