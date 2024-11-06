612be3ef355bd569393a95aba8728e496a0fd618
computej <computejpdc@gmail.com>
2024-11-06 09:05:25 -0800

    add a second date input

--- src/index.js
+++ src/index.js
@@ -11,8 +11,9 @@ window.addEventListener("load", function() {
   let ageForm = document.getElementById("age-form");
   ageForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    const inputDate = Date.parse(this.document.getElementById("birthday-input").value);
-    let ages = new UserAgeObject(new Date(inputDate), new Date(Date.now())).getOtherPlanetValue();
+    const firstDate = Date.parse(this.document.getElementById("firstdate-input").value);
+    const secondDate = Date.parse(this.document.getElementById("seconddate-input").value);
+    let ages = new UserAgeObject(new Date(firstDate), new Date(seconddate)).getOtherPlanetValue();
     let timespanTableElements =  document.querySelectorAll("#timespan-display td[id|=timespan]");
     let mapIndex = 0; // map foreach has key and value but no index indicator
     ages.forEach((value, key) => {
