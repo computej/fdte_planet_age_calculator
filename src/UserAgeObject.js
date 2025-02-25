694cc46c6e5f4eeae3f58c6bdef0a6aa66c4071f
computej <computejpdc@gmail.com>
2025-02-25 11:00:22 -0800

    rename variables in UserAgeObject

--- src/UserAgeObject.js
+++ src/UserAgeObject.js
@@ -1,7 +1,8 @@
 export class UserAgeObject {
-  //TODO: Numbers: not dates
+  //TODO: rename a few variables here. what to do with dateTwo?
+  //TODO: use only one argument
   constructor(dateOne,dateTwo) {
-    this.firstDate = dateOne;
+    this.inputAgeYears = dateOne;
     this.secondDate = 0;
     /*istanbul ignore else */ 
     if(!isNaN(dateTwo) &&  isFinite(dateTwo)) {
@@ -10,14 +11,14 @@ export class UserAgeObject {
   }
 
   getOtherPlanetAgeValues() {
-    const ageDifference = (this.secondDate - this.firstDate);
+    //const ageDifference = (this.secondDate - this.inputAgeYears);
     return new Map([
-      ["Earth", Math.abs(ageDifference)],
-      ["Mercury", Math.abs(ageDifference) * (1/.24)],
-      ["Venus",Math.abs(ageDifference) * (1/.62)],
-      ["Mars",Math.abs(ageDifference) * (1/1.88)],
-      ["Jupiter", Math.abs(ageDifference) * (1/11.86)],
-      ["futureOrPast", Math.sign(ageDifference) > 0 ? "Future Age" : "Past Age"]
+      ["Earth", Math.abs(this.inputAgeYears)],
+      ["Mercury", Math.abs(this.inputAgeYears) * (1/.24)],
+      ["Venus",Math.abs(this.inputAgeYears) * (1/.62)],
+      ["Mars",Math.abs(this.inputAgeYears) * (1/1.88)],
+      ["Jupiter", Math.abs(this.inputAgeYears) * (1/11.86)],
+      ["futureOrPast", Math.sign(this.inputAgeYears) > 0 ? "Future Age" : "Past Age"]
     ]);
   }
 }
