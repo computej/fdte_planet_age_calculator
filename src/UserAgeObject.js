533df83818e56efaffcdffb41c574db66cb1ff65
computej <computejpdc@gmail.com>
2025-03-03 10:09:25 -0800

    add getOtherPlanetYearsUntilSince()

--- src/UserAgeObject.js
+++ src/UserAgeObject.js
@@ -21,4 +21,15 @@ export class UserAgeObject {
       ["futureOrPast", Math.sign(this.inputAgeYears) > 0 ? "Future Age" : "Past Age"]
     ]);
   }
+
+  getOtherPlanetYearsUntilSince(compareAge) {
+    const ageDifference = compareAge - this.inputAgeYears;
+    return new Map([
+      ["Earth", ageDifference],
+      ["Mercury", ageDifference * (1/.24)],
+      ["Venus", ageDifference * (1/.62)],
+      ["Mars", ageDifference * (1/1.88)],
+      ["Jupiter", ageDifference * (1/11.86)]
+    ]);
+  }
 }
