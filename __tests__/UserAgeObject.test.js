4a338b8b6b2ccb500dcade80d08d15eb2b4ac161
computej <computejpdc@gmail.com>
2025-03-05 11:24:18 -0800

    code cleanup: remove comments, unnecessary vars and skipped tests

--- __tests__/UserAgeObject.test.js
+++ __tests__/UserAgeObject.test.js
@@ -1,11 +1,8 @@
 import { UserAgeObject } from '../src/UserAgeObject.js';
 
-//TODO: use numbers not dates
-
 const testFirstAge = 21;
 const testSecondAge = 23;
 
-const testDuration = testSecondAge - testFirstAge;
 let testObject = new UserAgeObject(testFirstAge,testSecondAge);
 
 describe("UserAgeObject", () => {
@@ -17,9 +14,6 @@ describe("UserAgeObject", () => {
   describe("constructor()", () => {
     test("It should write its first argument to the variable \"inputAgeYears\"", () => {
       expect(testObject.inputAgeYears).toStrictEqual(testFirstAge);
-    });
-    test.skip("It should write its second argument to \"secondDate\"", () => {
-      expect(testObject.secondDate).toStrictEqual(testSecondAge);
     });
   });
   describe("getOtherPlanetAgeValues()", () => {
@@ -47,17 +41,7 @@ describe("UserAgeObject", () => {
     test("The value of the \"Jupiter\" key is equal to 1/11.86 times the value of testFirstAge", () => {
       expect(testObject.getOtherPlanetAgeValues().get("Jupiter")).toBe(testFirstAge * (1/11.86));
     });
-
-    //may move to the new function
-    test.skip("if the difference between secondDate and firstDate is a positive value, write \"Future Age\" to futureOrPast", () => {
-      expect(testObject.getOtherPlanetAgeValues().get("futureOrPast")).toBe("Future Age");
-    });
-    test.skip("if the difference between secondDate and firstDate is a negative value or equal to 0, write \"Past Age\" to futureOrPast", () => {
-      let newTestObject = new UserAgeObject(testSecondAge,testFirstAge);
-      expect(newTestObject.getOtherPlanetAgeValues().get("futureOrPast")).toBe("Past Age");
-    });
   });
-  //TODO: New function to be added, computes years until or since a different age.
   describe("getOtherPlanetYearsUntilSince()", () => {
     test("It should return a set like object", () => {
       let testMap = testObject.getOtherPlanetYearsUntilSince(1);
--- src/UserAgeObject.js
+++ src/UserAgeObject.js
@@ -1,13 +1,6 @@
 export class UserAgeObject {
-  //TODO: rename a few variables here. what to do with dateTwo?
-  //TODO: use only one argument
-  constructor(dateOne,dateTwo) {
+  constructor(dateOne) {
     this.inputAgeYears = dateOne;
-    this.secondDate = 0;
-    /*istanbul ignore else */ 
-    if(!isNaN(dateTwo) &&  isFinite(dateTwo)) {
-      this.secondDate = dateTwo;
-    }
   }
 
   getOtherPlanetAgeValues() {
@@ -16,8 +9,7 @@ export class UserAgeObject {
       ["Mercury", this.inputAgeYears * (1/.24)],
       ["Venus",this.inputAgeYears * (1/.62)],
       ["Mars",this.inputAgeYears * (1/1.88)],
-      ["Jupiter", this.inputAgeYears * (1/11.86)],
-      ["futureOrPast", Math.sign(this.inputAgeYears) > 0 ? "Future Age" : "Past Age"]
+      ["Jupiter", this.inputAgeYears * (1/11.86)]
     ]);
   }
 
